e328f0ab92deda3051668d288b2f2fadc055d807
Ryan Liou <danny80916kimo@gmail.com>
2024-02-15 01:01:10 +0800

    Revert "Revert "Revert "add _site"""
    This reverts commit cb16a4720051f7b77d71011c331733847f4c1562.

--- docs/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome To Jekyll",
-        "excerpt":"# Title # test 123 layout: post title: “Welcome to Jekyll!” date: 2024-02-14 10:41:53 +0800 categories: jekyll update — You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but...","categories": [],
-        "tags": [],
-        "url": "/2024/02/14/welcome-to-jekyll.html",
-        "teaser": null
-      }]
